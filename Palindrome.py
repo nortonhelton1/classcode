6ec3e8725007daa575c96f1dad2fd167243bb82e
nortonhelton1 <87027439+nortonhelton1@users.noreply.github.com>
2021-07-29 15:50:36 -0400

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,26 @@
+user_input(Please enter word to determine if palindrome:")
+
+def is_Palindrome(user):
+    reversed_word = ""
+
+    for index in range(len(word)-1, -1, -1):
+        reversed_word + user_input[index]
+
+    if user_input == reverse_word: 
+
+        return True
+    else:
+        return False
+
+result = is palindrome(user_input)
+
+    If result
+
+       print("palindrome) 
+
+    else
+
+        print ("not")
+
+
+
